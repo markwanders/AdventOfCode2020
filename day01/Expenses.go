c953aafd032fa4b193ad75ac2492906c1a257778
Mark Wanders <mark.wanders@ing.com>
2020-12-01 10:39:46 +0100

    Minor performance improvements

--- day01/Expenses.go
+++ day01/Expenses.go
@@ -13,27 +13,24 @@ func main() {
 	lines := strings.Split(string(dat), "\n")
 	for i := 0; i < len(lines)-1; i++ {
 		a, _ := strconv.Atoi(lines[i])
-		for j := 0; j < len(lines)-1; j++ {
-			if j != i {
-				b, _ := strconv.Atoi(lines[j])
-				if a+b == 2020 {
-					fmt.Printf("%v plus %v makes 2020\n", a, b)
-					solution := a * b
-					fmt.Printf("Multiply these values to get the solution: %v\n", solution)
-				} else if a+b < 2020 {
-					for k := 0; k < len(lines)-1; k++ {
-						if k != i {
-							c, _ := strconv.Atoi(lines[k])
-							if a+b+c == 2020 {
-								fmt.Printf("%v plus %v plus %v makes 2020\n", a, b, c)
-								solution := a * b * c
-								fmt.Printf("Multiply these values to get the solution: %v\n", solution)
-							}
-						}
+		for j := i + 1; j < len(lines)-1; j++ {
+			b, _ := strconv.Atoi(lines[j])
+			if a+b == 2020 {
+				fmt.Printf("%v plus %v makes 2020\n", a, b)
+				solution := a * b
+				fmt.Printf("Multiply these values to get the solution to part 1: %v\n", solution)
+				break
+			} else if a+b < 2020 {
+				for k := j + 1; k < len(lines)-1; k++ {
+					c, _ := strconv.Atoi(lines[k])
+					if a+b+c == 2020 {
+						fmt.Printf("%v plus %v plus %v makes 2020\n", a, b, c)
+						solution := a * b * c
+						fmt.Printf("Multiply these values to get the solution to part 2: %v\n", solution)
+						break
 					}
 				}
 			}
-
 		}
 	}
 }
