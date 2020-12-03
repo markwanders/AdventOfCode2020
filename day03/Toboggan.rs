1e0ae567303dfb49506f6bc21d64e0bd41d012d9
Mark Wanders <mndmwanders@gmail.com>
2020-12-03 13:06:20 +0100

    Solved day 3 part 2 in Rust

--- day03/Toboggan.rs
+++ day03/Toboggan.rs
@@ -1,14 +1,20 @@
 fn main() {
     let contents = std::fs::read_to_string("input.txt").expect("should be there");
-    let mut x = 0;
-    let mut trees = 0;
-    for line in contents.lines() {
-       if line.chars().nth(x).unwrap() == '#' {
-           trees+=1;
-       }
-        x+=3;
-        x%=line.chars().count()
-    };
+    let mut answer :i64 = 1;
+    let slopes = [(1,1), (3,1), (5,1), (7,1),(1,2)];
+    for slope in &slopes {
+        let mut x = 0;
+        let mut trees = 0;
+        for line in contents.lines().step_by(slope.1) {
+            if line.chars().nth(x).unwrap() == '#' {
+                trees += 1;
+            }
+            x += slope.0;
+            x %= line.chars().count()
+        };
 
-    println!("{}", trees)
+        println!("({}, {}): {}", slope.0, slope.1, trees);
+        answer*=trees;
+    }
+    println!("{}", answer)
 }
