0324cbcc2a4c38dcccb6a7d6b994bee315813450
Mark Wanders <mark.wanders@ing.com>
2020-12-06 09:33:10 +0100

    Solved day 6 part 2

--- day06/customs.py
+++ day06/customs.py
@@ -2,8 +2,15 @@ import string
 
 with open("input.txt") as f:
     data = f.read().split("\n\n")
-    answers = [line.split("\n") for line in data]
-total = 0
-for answer in answers:
-    total += len(set(string.join(answer, "")))
-print total
+    forms = [line.split("\n") for line in data]
+part1 = 0
+part2 = 0
+for form in forms:
+    answers = set(string.join(form, ""))
+    part1 += len(answers)
+    for answer in answers:
+        if all(answer in line for line in form):
+            part2 += 1
+
+print part1
+print part2
