e6d30cbf804d91d3b2e12ece9ee63b187c94552c
Mark Wanders <mark.wanders@ing.com>
2020-12-07 12:53:14 +0100

    Day 7 part 1 now with proper recursion

--- day07/luggage.py
+++ day07/luggage.py
@@ -5,29 +5,28 @@ with open("input.txt") as f:
     parts = [line.split(" bags ") for line in data]
     rules = {part[0]: part[1:] for part in parts}
 
-bags_to_find = ["shiny gold"]
-found = True
-while found:
-    found = False
-    for bag in rules.keys():
-        for bag_to_find in bags_to_find:
-            if any(bag_to_find in rule for rule in rules[bag]) and bag not in bags_to_find:
-                bags_to_find.append(bag)
-                found = True
-print(len(bags_to_find) - 1)
+
+def find_containing_colors(inner_bag, containing_colors):
+    for outer_bag in rules.keys():
+        if any(inner_bag in rule for rule in rules[outer_bag]):
+            containing_colors[outer_bag] = 1
+            find_containing_colors(outer_bag, containing_colors)
+    return containing_colors
 
 
-def traverse(bag, number_of_bags):
+print(len(find_containing_colors("shiny gold", {})))
+
+
+def total_bags(bag, number_of_bags):
     additional_bags = 0
     for rule in rules[bag]:
         matches = re.findall("(?:contain|,) (.*?) (?:bag|bags)", rule)
-        # print("%s, %s" % (bag, matches))
         for match in matches:
             if "no other" not in match:
                 number = int(match[0])
                 contained_bag = match[2:]
-                additional_bags += traverse(contained_bag, number * number_of_bags)
+                additional_bags += total_bags(contained_bag, number * number_of_bags)
     return number_of_bags + additional_bags
 
 
-print(traverse("shiny gold", 1) - 1)
+print(total_bags("shiny gold", 1) - 1)
