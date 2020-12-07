24bea3274b39fa7faf410ae875f07b01389bd33d
Mark Wanders <mark.wanders@ing.com>
2020-12-07 13:52:48 +0100

    Day 7 minor cleanup

--- day07/luggage.py
+++ day07/luggage.py
@@ -1,27 +1,25 @@
 import re
 
 with open("input.txt") as f:
-    data = f.readlines()
-    parts = [line.split(" bags ") for line in data]
+    parts = [line.split(" bags ") for line in f.readlines()]
     rules = {part[0]: part[1:] for part in parts}
 
 
 def find_containing_colors(inner_bag, containing_colors):
     for outer_bag in rules.keys():
         if any(inner_bag in rule for rule in rules[outer_bag]):
-            containing_colors[outer_bag] = 1
+            containing_colors.add(outer_bag)
             find_containing_colors(outer_bag, containing_colors)
     return containing_colors
 
 
-print(len(find_containing_colors("shiny gold", {})))
+print("Part 1 : %s" % len(find_containing_colors("shiny gold", set())))
 
 
 def total_bags(bag, number_of_bags):
     additional_bags = 0
     for rule in rules[bag]:
-        matches = re.findall("(?:contain|,) (.*?) (?:bag|bags)", rule)
-        for match in matches:
+        for match in re.findall("(?:contain|,) (.*?) (?:bag|bags)", rule):
             if "no other" not in match:
                 number = int(match[0])
                 contained_bag = match[2:]
@@ -29,4 +27,4 @@ def total_bags(bag, number_of_bags):
     return number_of_bags + additional_bags
 
 
-print(total_bags("shiny gold", 1) - 1)
+print("Part 2: %s " % (total_bags("shiny gold", 1) - 1))
