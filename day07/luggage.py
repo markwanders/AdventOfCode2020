c453513deaf4ca6d0395455de608b0c0d33b1074
Mark Wanders <mark.wanders@ing.com>
2020-12-07 11:12:59 +0100

    Solved day 7 part 2

--- day07/luggage.py
+++ day07/luggage.py
@@ -1,6 +1,8 @@
+import re
+
 with open("input.txt") as f:
     data = f.readlines()
-    parts = [line.split(" bags") for line in data]
+    parts = [line.split(" bags ") for line in data]
     rules = {part[0]: part[1:] for part in parts}
 
 bags_to_find = ["shiny gold"]
@@ -13,3 +15,19 @@ while found:
                 bags_to_find.append(bag)
                 found = True
 print(len(bags_to_find) - 1)
+
+
+def traverse(bag, number_of_bags):
+    additional_bags = 0
+    for rule in rules[bag]:
+        matches = re.findall("(?:contain|,) (.*?) (?:bag|bags)", rule)
+        # print("%s, %s" % (bag, matches))
+        for match in matches:
+            if "no other" not in match:
+                number = int(match[0])
+                contained_bag = match[2:]
+                additional_bags += traverse(contained_bag, number * number_of_bags)
+    return number_of_bags + additional_bags
+
+
+print(traverse("shiny gold", 1) - 1)
