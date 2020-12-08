54cd2897115cdae8310f77d02abbcbfaaf9be7fd
Mark Wanders <mark.wanders@ing.com>
2020-12-08 09:17:49 +0100

    Solved day 8 part 2

--- day08/handheld.py
+++ day08/handheld.py
@@ -1,21 +1,45 @@
 with open("input.txt") as f:
     instructions = [line.strip().split(" ") for line in f.readlines()]
 
-indeces = set()
-accumulator, index = 0, 0
-while True:
-    instruction = instructions[index][0]
-    argument = int(instructions[index][1])
-    if instruction == "acc":
-        accumulator += argument
-        index += 1
-    elif instruction == "jmp":
-        index += argument
-    elif instruction == "nop":
-        index += 1
-    else:
-        print("Unrecognized instruction: %s at index %s" % (instruction[index], index))
-    if index in indeces:
-        break
-    indeces.add(index)
-print(accumulator)
+
+def run_program(instr):
+    indices = set()
+    accumulator, index = 0, 0
+    while True:
+        instruction = instr[index][0]
+        argument = int(instr[index][1])
+        if instruction == "acc":
+            accumulator += argument
+            index += 1
+        elif instruction == "jmp":
+            index += argument
+        elif instruction == "nop":
+            index += 1
+        else:
+            print("Unrecognized instruction: %s at index %s" % (instruction[index], index))
+        if index >= len(instr):
+            print("Tried to access operation beyond program memory: %s %s" % (index, accumulator))
+            break
+        if index in indices:
+            break
+        indices.add(index)
+    return accumulator
+
+
+print(run_program(instructions))
+
+
+def variate(instr):
+    nopjmp = [i for i in instr if i[0] == "jmp" or i[0] == "nop"]
+    for op in nopjmp:
+        changed_instructions = instr.copy()
+        op_index = instr.index(op)
+        if op[0] == "jmp":
+            changed_instructions[op_index] = ["nop", op[1]]
+            run_program(changed_instructions)
+        elif op[0] == "nop":
+            changed_instructions[op_index] = ["jmp", op[1]]
+            run_program(changed_instructions)
+
+
+variate(instructions)
