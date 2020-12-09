d3fa442e5cbab3b4419df427d9229ff29b831ab4
Mark Wanders <mndmwanders@gmail.com>
2020-12-09 09:03:56 +0100

    Solved day 9 part 2

--- day09/encoding.py
+++ day09/encoding.py
@@ -1,11 +1,21 @@
 with open("input.txt") as f:
     data = [int(s) for s in f.readlines()]
 
+part1 = 0
 for i in range(25, len(data)):
-    preamble = data[i-25:i]
+    preamble = data[i - 25:i]
     if any(j + k == data[i] and j != k for j in preamble for k in preamble):
         continue
     else:
-        print(data[i])
+        part1 = data[i]
+        break
+print(part1)
+
+for i in range(0, len(data)):
+    sequence = []
+    while sum(sequence) < part1:
+        sequence.append(data[i + len(sequence)])
+    if sum(sequence) == part1:
+        print(min(sequence) + max(sequence))
         break
 
