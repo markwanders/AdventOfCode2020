312b5461546039b3a3630c906488a07e323a8fc5
Mark Wanders <mndmwanders@gmail.com>
2020-12-10 12:21:02 +0100

    This should work for day 10 part 2 but doesnt...

--- day10/jolts.py
+++ day10/jolts.py
@@ -1,14 +1,23 @@
 with open("input.txt") as f:
     data = [int(s) for s in f.readlines()]
+    data.append(max(data) + 3)
     data.sort()
 
 one = 0
-three = 1
+three = 0
 current = 0
-for index, adapter in enumerate(data):
+for adapter in data:
     if adapter - current == 1:
         one += 1
     if adapter - current == 3:
         three += 1
     current = adapter
 print(one, three, one * three)
+
+paths = {}
+for adapter in data:
+    if adapter == min(data):
+        paths[adapter] = 1
+    else:
+        paths[adapter] = paths.get(adapter - 1, 0) + paths.get(adapter - 2, 0) + paths.get(adapter - 3, 0)
+print(max(paths.values()))
