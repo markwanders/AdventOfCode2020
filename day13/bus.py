f588974cf640e83f28df9ad4a22154dd7e5ae6e8
Mark Wanders <mndmwanders@gmail.com>
2020-12-13 12:29:34 +0100

    Solved day 13 part 2

--- day13/bus.py
+++ day13/bus.py
@@ -13,3 +13,23 @@ def part1():
 
 
 print(part1())
+
+
+def part2():
+    t = 0
+    step = 0
+    previous_bus = 0
+    previous_offset = 0
+    for bus in [int(b) for b in busses if b.isdigit()]:
+        offset = busses.index(str(bus))
+        if previous_bus > 0:
+            while (t + offset) % bus != 0 or (t + previous_offset) % previous_bus != 0:
+                t += step
+            step *= bus
+        else:
+            step = bus
+        previous_bus, previous_offset = bus, offset
+    return t
+
+
+print(part2())
