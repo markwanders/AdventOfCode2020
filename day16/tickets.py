18996b7a1a2d99ce7d0dd33a0901943e55e80a28
Mark Wanders <mark.wanders@ing.com>
2020-12-16 09:48:35 +0100

    Solved day 16 part 2

--- day16/tickets.py
+++ day16/tickets.py
@@ -1,4 +1,6 @@
 import re
+from functools import reduce
+import operator
 
 with open("input.txt") as f:
     parts = f.read().split("\n\n")
@@ -10,15 +12,42 @@ for line in parts[0].split("\n"):
     range2 = [int(i) for i in match[2].split("-")]
     allowed_numbers.update(set(range(range1[0], range1[1] + 1)))
     allowed_numbers.update(set(range(range2[0], range2[1] + 1)))
-    fields[match[0]] = ([int(i) for i in match[1].split("-")], [int(i) for i in match[2].split("-")])
+    fields[match[0]] = set(range(range1[0], range1[1] + 1))
+    fields[match[0]].update(set(range(range2[0], range2[1] + 1)))
+tickets = parts[2].split("\n")[1:]
+your_ticket = [int(i) for i in parts[1].split("\n")[-1].split(",")]
 
-print(allowed_numbers)
-errors = []
-for line in parts[2].split("\n"):
-    if "nearby tickets" in line:
-        continue
-    numbers = [int(i) for i in line.split(",")]
-    for number in numbers:
-        if number not in allowed_numbers:
-            errors.append(number)
-print(sum(errors))
+
+def part1():
+    errors = []
+    for ticket in tickets:
+        for number in [int(i) for i in ticket.split(",")]:
+            if number not in allowed_numbers:
+                errors.append(number)
+    print(sum(errors))
+
+
+def part2():
+    valid_tickets = []
+    for ticket in tickets:
+        if all(number in allowed_numbers for number in [int(i) for i in ticket.split(",")]):
+            valid_tickets.append(ticket)
+    possibilities = [list(fields.keys()) for i in range(len(fields.keys()))]
+    for ticket in valid_tickets:
+        for i, number in enumerate([int(i) for i in ticket.split(",")]):
+            for field, field_range in fields.items():
+                if number not in field_range:
+                    possibilities[i].remove(field)
+    actual_fields = ["empty"] * len(fields.keys())
+    while "empty" in actual_fields:
+        for i, possibility in enumerate(possibilities):
+            for field in possibility:
+                if [p for sublist in possibilities for p in sublist].count(field) == 1 and field not in actual_fields:
+                    actual_fields[i] = field
+                    possibilities[i] = []
+                    break
+    print(reduce(operator.mul, [value for (i, value) in enumerate(your_ticket) if "departure" in actual_fields[your_ticket.index(value)]], 1))
+
+
+part1()
+part2()
