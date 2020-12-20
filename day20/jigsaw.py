8edaddfcd855998c24c972ad5aee44e3b66ed634
Mark Wanders <mndmwanders@gmail.com>
2020-12-20 15:28:35 +0100

    Solved day 20 part 1

--- day20/jigsaw.py
+++ day20/jigsaw.py
@@ -2,57 +2,32 @@ with open("input.txt") as f:
     original_pieces = [l.split("\n") for l in f.read().split("\n\n")]
 
 
-def print_tile(tile):
-    for line in tile:
-        print("%s" % line)
-    print("\n")
-
-
-def to_number(line):
-    return int(line.replace(".", "0").replace("#", "1"), 2)
-
-
 def transpose_tile(tile):
     return ["".join(t) for t in list(map(list, zip(*tile)))]
 
 
-def flip_tile_horizontally(tile):
-    return [s[::-1] for s in tile]
+matching_sides = dict()
+for piece in original_pieces:
+    tile = piece[1:]
+    tile_id = int(piece[0][-5:-1])
+    if tile_id not in matching_sides.keys():
+        matching_sides[tile_id] = []
+    transposed_tile = transpose_tile(tile)
+    edges = [tile[0], tile[-1], transposed_tile[0], transposed_tile[-1]]
+    for other_piece in original_pieces:
+        other_tile = other_piece[1:]
+        other_tile_id = int(other_piece[0][-5:-1])
+        if tile_id != other_tile_id and other_tile_id not in matching_sides[tile_id] and tile_id not in matching_sides.get(other_tile_id, []):
+            if other_tile_id not in matching_sides.keys():
+                matching_sides[other_tile_id] = []
+            transposed_other_tile = transpose_tile(other_tile)
+            other_edges = [other_tile[0], other_tile[-1], transposed_other_tile[0], transposed_other_tile[-1]]
+            if any(s for s in edges if s in other_edges or s in [r[::-1] for r in other_edges]) > 0:
+                matching_sides[tile_id].append(other_tile_id)
+                matching_sides[other_tile_id].append(tile_id)
 
-
-def flip_tile_vertically(tile):
-    return transpose_tile([s[::-1] for s in transpose_tile(tile)])
-
-
-def permutations(pieces):
-    permutations = {}
-    for piece in pieces:
-        header = piece[0]
-        tile = piece[1:]
-        tile_id = int(header[-5:-1])
-        permutations[tile_id] = []
-        permutations[tile_id] += [flip_tile_horizontally(tile)]
-        permutations[tile_id] += [flip_tile_vertically(tile)]
-        permutations[tile_id] += [flip_tile_vertically(flip_tile_horizontally(tile))]
-    return permutations
-
-
-tops, bottoms, lefts, rights = dict(), dict(), dict(), dict()
-pieces = permutations(original_pieces)
-print(pieces)
-for tile_id, permutations in pieces.items():
-    tops[tile_id], bottoms[tile_id], lefts[tile_id], rights[tile_id] = [], [], [], []
-    for tile in permutations:
-        top = tile[0]
-        bottom = tile[-1]
-        transposed_tile = transpose_tile(tile)
-        left = transposed_tile[0]
-        right = transposed_tile[-1]
-        tops[tile_id] += [to_number(top)]
-        bottoms[tile_id] += [to_number(bottom)]
-        lefts[tile_id] += [to_number(left)]
-        rights[tile_id] += [to_number(right)]
-
-
-print(bottoms, tops, lefts, rights)
-
+corners = list(k for k, v in matching_sides.items() if len(v) == 2)
+answer = 1
+for corner in corners:
+    answer *= corner
+print(answer)
