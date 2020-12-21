78bdf23eb4cdb81024fc8d33eb3b26cbbf0ba240
Mark Wanders <mark.wanders@ing.com>
2020-12-21 15:14:07 +0100

    Day 21 minor cleanup

--- day21/allergens.py
+++ day21/allergens.py
@@ -4,26 +4,22 @@ with open("input.txt") as f:
     foods = f.read().splitlines()
 
 all_ingredients = set()
-all_foods = set()
 allergens_in_food = dict()
 for food in foods:
-    ingredients, allergens = map(
-        lambda x: x.replace("(", "").replace(")", "").replace("contains ", "").replace(",", "").split(" "),
-        food.split(" ("))
+    ingredients, allergens = map(lambda x: x.replace(",", "").split(" "), food.rstrip(")").split(" (contains "))
     all_ingredients.update(ingredients)
-    all_foods.add(" ".join(ingredients))
     for allergen in allergens:
-        if allergen in allergens_in_food.keys():
+        if allergen in allergens_in_food:
             allergens_in_food[allergen].append(ingredients)
         else:
             allergens_in_food[allergen] = [ingredients]
 contains_allergens = set()
 for allergen, list_of_food in allergens_in_food.items():
-    contains_allergens.update(set.intersection(*map(set, list_of_food)))
+    contains_allergens |= set.intersection(*map(set, list_of_food))
 ingredients_without_allergens = [i for i in all_ingredients if i not in contains_allergens]
 count = 0
 for ingredient in ingredients_without_allergens:
-    for food in all_foods:
+    for food in foods:
         pattern = fr"(?:^| )({ingredient})(?:$| )"
         count = count + 1 if re.search(pattern, food) else count
 print(count)
