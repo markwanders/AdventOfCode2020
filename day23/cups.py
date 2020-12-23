313299d49dcdf0ea845d1637dfc9c804af1f4cd2
Mark Wanders <mndmwanders@gmail.com>
2020-12-23 15:19:00 +0100

    Solved day 23 part 2

--- day23/cups.py
+++ day23/cups.py
@@ -1,26 +1,69 @@
 from collections import deque
 
 with open("input.txt") as f:
-    cups = deque([int(c) for c in f.readline()])
+    cups = [int(c) for c in f.readline()]
 
-for i in range(100):
-    current_value = cups[0]
-    # print("--- Move %s ---" % (i + 1))
-    # print("Current cup is %s, list is %s" % (current_value, cups))
-    cups.rotate(cups.index(current_value) - 1)
-    pickup = [cups.popleft()] + [cups.popleft()] + [cups.popleft()]
-    # print("Picked up %s, list is %s" % (pickup, cups))
-    destination_value = current_value - 1
-    while destination_value not in cups:
-        destination_value -= 1
-        if destination_value < min(cups):
-            destination_value = max(cups)
-    destination = cups.index(destination_value)
-    # print("Destination cup %s at %s" % (destination_value, destination))
-    pickup.reverse()
-    for p in pickup:
-        cups.insert(destination + 1, p)
-    # print("Inserted cups, list is now %s" % cups)
-    current = cups.index(current_value) + 1
-cups.rotate(-cups.index(1))
-print("".join([str(c) for c in cups][1:]))
+
+def part1(cups):
+    for _ in range(100):
+        current_value = cups[0]
+        cups.rotate(cups.index(current_value) - 1)
+        pickup = [cups.popleft()] + [cups.popleft()] + [cups.popleft()]
+        destination_value = current_value - 1
+        while destination_value not in cups:
+            destination_value -= 1
+            if destination_value < min(cups):
+                destination_value = max(cups)
+        destination = cups.index(destination_value)
+        pickup.reverse()
+        for p in pickup:
+            cups.insert(destination + 1, p)
+    cups.rotate(-cups.index(1))
+    print("".join([str(c) for c in cups][1:]))
+
+
+part1(deque(cups))
+
+
+class Link:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def part2(cups):
+    cup_range = int(1e6)
+    links = {}
+
+    for c in range(1, cup_range + 1):
+        links[c] = Link(c)
+
+    for a, b in zip(cups, cups[1:]):
+        links[a].next = links[b]
+
+    links[cups[-1]].next = links[len(cups) + 1]
+    for i in range(len(cups) + 1, cup_range):
+        links[i].next = links[i + 1]
+    links[cup_range].next = links[cups[0]]
+
+    current = links[cups[0]]
+
+    for _ in range(int(1e7)):
+        pickup_start = current.next
+        pickup_end = current.next.next.next
+        pickup = [pickup_start.value, pickup_start.next.value, pickup_end.value]
+        destination = current.value - 1 if current.value > 1 else cup_range
+        while destination in pickup:
+            destination -= 1
+            if destination == 0:
+                destination = cup_range
+
+        current.next = pickup_end.next
+        pickup_end.next = links[destination].next
+        links[destination].next = pickup_start
+
+        current = current.next
+    print(links[1].next.value * links[1].next.next.value)
+
+
+part2(cups.copy())
