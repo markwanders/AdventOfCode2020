cfcf56cb5daba896c692cbbb5582268468eec81b
Mark Wanders <mark.wanders@ing.com>
2020-12-24 16:15:24 +0100

    Solved day 24 part 2

--- day24/tiles.py
+++ day24/tiles.py
@@ -26,3 +26,23 @@ for line in lines:
     else:
         tiles[pos] = True
 print(sum(v for v in tiles.values()))
+
+
+def neighbors(tile):
+    return [tile + (2 + 0j), tile + (-2 + 0j), tile + (1 + 1j), tile + (-1 - 1j), tile + (1 - 1j), tile + (-1 + 1j)]
+
+
+for i in range(100):
+    old_tiles = dict(tiles)
+    tiles_to_check = list(tiles.keys())
+    while len(tiles_to_check) > 0:
+        tile = tiles_to_check.pop()
+        neighboring_tiles = neighbors(tile)
+        black_neighbors = sum(old_tiles.get(n, False) for n in neighboring_tiles)
+        if old_tiles.get(tile, False):
+            if black_neighbors == 0 or black_neighbors > 2:
+                tiles[tile] = False
+            tiles_to_check += list(t for t in neighboring_tiles if t not in tiles.keys())
+        elif not old_tiles.get(tile, False) and black_neighbors == 2:
+            tiles[tile] = True
+print(sum(v for v in tiles.values()))
