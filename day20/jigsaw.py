ecc55a462225a6253e9aa76073c2c78b31a737d6
Mark Wanders <mndmwanders@gmail.com>
2021-08-09 16:52:07 +0200

    solved day 20 part 2

--- day20/jigsaw.py
+++ day20/jigsaw.py
@@ -1,3 +1,6 @@
+import math
+import re
+
 with open("input.txt") as f:
     original_pieces = [l.split("\n") for l in f.read().split("\n\n")]
 
@@ -26,6 +29,10 @@ def rotate_flip(tile):
     return rotate(flip(tile))
 
 
+def rotate_v_flip(tile):
+    return rotate(v_flip(tile))
+
+
 def rotate180_flip(tile):
     return rotate180(flip(tile))
 
@@ -42,6 +49,14 @@ def print_tile(tile):
 def trim_edges(tile):
     return [c[1:-1] for c in tile[1:-1]]
 
+
+def find_sea_monsters(completed_puzzle):
+    k = len(completed_puzzle[0])
+    for operation in operations:
+        image = "\n".join(operation(completed_puzzle))
+        monsters = len(re.findall('(?=(#..{'+str(k-19)+'}#.{4}##.{4}##.{4}###.{'+str(k-19)+'}.#.{2}#.{2}#.{2}#.{2}#.{2}#.{3}))', image, re.DOTALL))
+        if monsters > 0:
+            return monsters
 
 matching_sides = dict()
 tiles = dict()
@@ -71,70 +86,66 @@ for corner in corners:
     answer *= corner
 print(answer)
 
+puzzle_size = int(math.sqrt(len(tiles)))
+
 
 def find_vertical_match(tile_id):
     tile = tiles[tile_id]
     for adjacent_tile_id in matching_sides[tile_id]:
         adjacent_tile = tiles[adjacent_tile_id]
         if tile[-1] == adjacent_tile[0]:  # if the bottom of current tile matches top of adjacent tile
-            # print("Found matching top and bottom!")
             matching_sides[tile_id].remove(adjacent_tile_id)  # remove these tile_ids from the list to solve
             matching_sides[adjacent_tile_id].remove(tile_id)
             return adjacent_tile_id
         else:
             for operation in operations:
                 if tile[-1] == operation(adjacent_tile)[0]:
-                    # print(f"Found matching top and bottom after {operation.__name__}!")
                     matching_sides[tile_id].remove(adjacent_tile_id)
                     matching_sides[adjacent_tile_id].remove(tile_id)
                     tiles[adjacent_tile_id] = operation(adjacent_tile)  # put in correct position
                     return adjacent_tile_id
-    print(f"No vertical match found for tile {tile_id}")
 
 
 def find_horizontal_match(tile_id):
     tile = tiles[tile_id]
     for adjacent_tile_id in matching_sides[tile_id]:
         adjacent_tile = tiles[adjacent_tile_id]
-        if rotate(current_tile)[-1] == rotate(adjacent_tile)[0]:  # if the left of current tile matches right of adjacent tile
-            # print("Found matching left and right!")
+        if rotate(tile)[-1] == rotate(adjacent_tile)[0]:  # if the left of current tile matches right of adjacent tile
             matching_sides[tile_id].remove(adjacent_tile_id)  # remove these tile_ids from the list to solve
             matching_sides[adjacent_tile_id].remove(tile_id)
             return adjacent_tile_id
         else:
             for operation in operations:
                 if rotate(tile)[-1] == rotate(operation(adjacent_tile))[0]:
-                    # print(f"Found matching left and right after {operation.__name__}!")
                     matching_sides[tile_id].remove(adjacent_tile_id)
                     matching_sides[adjacent_tile_id].remove(tile_id)
                     tiles[adjacent_tile_id] = operation(adjacent_tile)  # put in correct position
                     return adjacent_tile_id
-    print(f"No horizontal match found for tile {tile_id}")
 
 
 def add_tile_to_puzzle(_tile_id, _puzzle, insert_height):
-    if len(_puzzle) < 8 * 12:  # if we haven't completed a single column yet, just add the tile to the bottom
+    if len(_puzzle) < 8 * puzzle_size:  # if we haven't completed a single column yet, just add the tile to the bottom
         _puzzle += trim_edges(tiles[_tile_id])
     else:
         tile_to_add = trim_edges(tiles[_tile_id])
         insert_height = (insert_height + 1) * 8
         for i, line in enumerate(tile_to_add):
             _puzzle[insert_height + i] += line
-    print_tile(_puzzle)
 
 
-current_tile_id = corners[1]
+current_tile_id = corners[1]  # top left corner found by trial and error, should improve
 column_top = current_tile_id
 puzzle = trim_edges(tiles[current_tile_id])
-operations = [rotate, rotate180, rotate270, flip, rotate_flip, rotate180_flip, rotate270_flip]
+operations = [rotate, rotate180, rotate270, flip, rotate_flip, rotate180_flip, rotate270_flip, v_flip, rotate_v_flip]
 
-for x in range(0, 11):
-    print(f"x: {x}")
-    for y in range(0, 11):
-        print(f"y: {y}")
+for x in range(0, puzzle_size):
+    for y in range(0, puzzle_size - 1):
         current_tile = tiles[current_tile_id]
         matching_tile_id = current_tile_id = find_vertical_match(current_tile_id)
         add_tile_to_puzzle(current_tile_id, puzzle, y)
     column_top = current_tile_id = find_horizontal_match(column_top)
-    add_tile_to_puzzle(current_tile_id, puzzle, -1)
+    if current_tile_id:
+        add_tile_to_puzzle(current_tile_id, puzzle, -1)
 
+pixels = sum([line.count('#') for line in puzzle])
+print(pixels - find_sea_monsters(puzzle) * 15)  # 16 pixels per sea monster
