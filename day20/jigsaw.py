686cba65aa19499a7410bcb138fa16f99c9a3f6a
Mark Wanders <mndmwanders@gmail.com>
2021-08-09 14:42:26 +0200

    fillin puzzle horizontally wip

--- day20/jigsaw.py
+++ day20/jigsaw.py
@@ -71,34 +71,70 @@ for corner in corners:
     answer *= corner
 print(answer)
 
-current_tile_id = corners[1]
-column_top = tiles[current_tile_id]
-puzzle = trim_edges(column_top)
-operations = [rotate, rotate180, rotate270, flip, rotate_flip, rotate180_flip, rotate270_flip]
 
-i = 1
-while i < 14:
-    current_tile = tiles[current_tile_id]
-    print(current_tile_id)
-    print(matching_sides[current_tile_id])
-    for adjacent_tile_id in matching_sides[current_tile_id]:
+def find_vertical_match(tile_id):
+    tile = tiles[tile_id]
+    for adjacent_tile_id in matching_sides[tile_id]:
         adjacent_tile = tiles[adjacent_tile_id]
-        if current_tile[-1] == adjacent_tile[0]:  # if the bottom of current tile matches top of adjacent tile
-            print("Found matching top and bottom!")
-            puzzle += trim_edges(adjacent_tile)  # then add it to the bottom of the puzzle, current column
-            matching_sides[current_tile_id].remove(adjacent_tile_id)  # remove these tile_ids from the list to solve
-            matching_sides[adjacent_tile_id].remove(current_tile_id)
-            current_tile_id = adjacent_tile_id
-            break
+        if tile[-1] == adjacent_tile[0]:  # if the bottom of current tile matches top of adjacent tile
+            # print("Found matching top and bottom!")
+            matching_sides[tile_id].remove(adjacent_tile_id)  # remove these tile_ids from the list to solve
+            matching_sides[adjacent_tile_id].remove(tile_id)
+            return adjacent_tile_id
         else:
             for operation in operations:
-                if current_tile[-1] == operation(adjacent_tile)[0]:
-                    print(f"Found matching top and bottom after {operation.__name__}!")
-                    puzzle += trim_edges(operation(adjacent_tile))
-                    matching_sides[current_tile_id].remove(adjacent_tile_id)
-                    matching_sides[adjacent_tile_id].remove(current_tile_id)
+                if tile[-1] == operation(adjacent_tile)[0]:
+                    # print(f"Found matching top and bottom after {operation.__name__}!")
+                    matching_sides[tile_id].remove(adjacent_tile_id)
+                    matching_sides[adjacent_tile_id].remove(tile_id)
                     tiles[adjacent_tile_id] = operation(adjacent_tile)  # put in correct position
-                    current_tile_id = adjacent_tile_id
-                    break
-    i += 1
-print_tile(puzzle)
+                    return adjacent_tile_id
+    print(f"No vertical match found for tile {tile_id}")
+
+
+def find_horizontal_match(tile_id):
+    tile = tiles[tile_id]
+    for adjacent_tile_id in matching_sides[tile_id]:
+        adjacent_tile = tiles[adjacent_tile_id]
+        if rotate(current_tile)[-1] == rotate(adjacent_tile)[0]:  # if the left of current tile matches right of adjacent tile
+            # print("Found matching left and right!")
+            matching_sides[tile_id].remove(adjacent_tile_id)  # remove these tile_ids from the list to solve
+            matching_sides[adjacent_tile_id].remove(tile_id)
+            return adjacent_tile_id
+        else:
+            for operation in operations:
+                if rotate(tile)[-1] == rotate(operation(adjacent_tile))[0]:
+                    # print(f"Found matching left and right after {operation.__name__}!")
+                    matching_sides[tile_id].remove(adjacent_tile_id)
+                    matching_sides[adjacent_tile_id].remove(tile_id)
+                    tiles[adjacent_tile_id] = operation(adjacent_tile)  # put in correct position
+                    return adjacent_tile_id
+    print(f"No horizontal match found for tile {tile_id}")
+
+
+def add_tile_to_puzzle(_tile_id, _puzzle, insert_height):
+    if len(_puzzle) < 8 * 12:  # if we haven't completed a single column yet, just add the tile to the bottom
+        _puzzle += trim_edges(tiles[_tile_id])
+    else:
+        tile_to_add = trim_edges(tiles[_tile_id])
+        insert_height = (insert_height + 1) * 8
+        for i, line in enumerate(tile_to_add):
+            _puzzle[insert_height + i] += line
+    print_tile(_puzzle)
+
+
+current_tile_id = corners[1]
+column_top = current_tile_id
+puzzle = trim_edges(tiles[current_tile_id])
+operations = [rotate, rotate180, rotate270, flip, rotate_flip, rotate180_flip, rotate270_flip]
+
+for x in range(0, 11):
+    print(f"x: {x}")
+    for y in range(0, 11):
+        print(f"y: {y}")
+        current_tile = tiles[current_tile_id]
+        matching_tile_id = current_tile_id = find_vertical_match(current_tile_id)
+        add_tile_to_puzzle(current_tile_id, puzzle, y)
+    column_top = current_tile_id = find_horizontal_match(column_top)
+    add_tile_to_puzzle(current_tile_id, puzzle, -1)
+
